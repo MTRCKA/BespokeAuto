cf1a967a485ef3df11fab6f55542ebfffe626873
Michael T <mtrcka@Michaels-MacBook-Pro.local>
2023-11-20 11:12:35 -0600

    removed graphQL and added email service

--- BespokeAuto-Web/src/components/ProductPricing.jsx
+++ BespokeAuto-Web/src/components/ProductPricing.jsx
@@ -269,7 +269,14 @@ export default function Example() {
             <form onSubmit={async (event) => {
               event.preventDefault(); 
               console.log(event.target[0].value);
-              intCreate(event.target[0].value + ' ' + event.target[1].value,event.target[2].value,event.target[3].value,event.target[4].value,event.target[5].value);
+              // intCreate(event.target[0].value + ' ' + event.target[1].value,event.target[2].value,event.target[3].value,event.target[4].value,event.target[5].value);
+              var templateParams = {
+                name: event.target[0].value + ' ' + event.target[1].value,
+                email: event.target[2].value,
+                country: event.target[3].value,
+                note:event.target[4].value
+              }
+              emailjs.send("service_ejuwjrh", "template_fs004kn", templateParams)
               setShowForm(false);
               }} id='form' className="w-full max-w-lg">
               <div className="flex flex-wrap -mx-3 mb-6">
@@ -301,11 +308,11 @@ export default function Example() {
                   </label>
                   <input required={true} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="country" type="text" placeholder="USA" />
                 </div>
-                <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                  {/* <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                {/* <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                  <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                     Product
-                  </label> */}
-                  {/* <div className="relative">
+                  </label>
+                  <div className="relative">
                     <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="product">
                       <option>Camaro RS Grille</option>
                       <option>Firebird Grille</option>
@@ -314,8 +321,8 @@ export default function Example() {
                     <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                       <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
                     </div>
-                  </div> */}
-                </div>
+                  </div>
+                </div> */}
               </div>
               <div className="flex flex-wrap -mx-3 mb-2">  
                 <div className="w-full px-3 mb-6 md:mb-0">
